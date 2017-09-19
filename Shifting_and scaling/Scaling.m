1eee12eac91786fa4cef7df9a362627c68c95f06
adithyahosapate <adihosapate@gmail.com>
2017-09-19 23:24:37 +0530

    changed Scaling

--- Shifting_and scaling/Scaling.m
+++ Shifting_and scaling/Scaling.m
@@ -12,15 +12,3 @@ audiowrite('output_file.aiff',B,fs);
 % the audio file will feel sped up by a factor of 2 as we are playing half the samples at the same playback rate
 %If you play the audio at x0.5 speed, you will get a slightly distorted ,but similar audio file.
 
-channel1=B(1:5050,1);
-
-points=linspace(1,50,length(channel1));
-
-plot(points,channel1);
-hold on
-channel2=x(1:5050,1);
-
-points=linspace(1,50,length(channel2));
-
-
-plot(points,channel2,'green');
