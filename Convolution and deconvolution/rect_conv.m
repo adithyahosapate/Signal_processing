4bcb1fdfdcdd083038c9c8d48b01ba6ce2c72e24
adithyahosapate <adihosapate@gmail.com>
2017-09-27 23:47:12 +0530

    added file

--- Convolution and deconvolution/rect_conv.m
+++ Convolution and deconvolution/rect_conv.m
@@ -1,13 +1,4 @@
-b=linspace(-10,10,1000);
-a=linspace(-10,10,1000);
-Z=sinc(a);
+b=linspace(1,2,1000);
+a=linspace(1,3,1000);
 c=conv(a,b);
-#plot(linspace(min(c),max(c),length(c)),c);
-A=fft(Z);
-B=fft(b);
-#plot(Z);
-plot(linspace(1,2,length(A)),abs(A)/length(A));
-A=linspace(10,10,1000);
-
-Z=ifft(A)
-plot(linspace(min(Z),max(Z),length(Z)),Z);
+plot(linspace(min(c),max(c),length(c)),c);
