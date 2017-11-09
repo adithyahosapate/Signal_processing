f2ce3f221ba768197b4776d9a18cf576e55a3661
adithyahosapate <adihosapate@gmail.com>
2017-11-09 22:28:43 +0530

    added the visualization

--- constant-Q-transform/visualize.py
+++ constant-Q-transform/visualize.py
@@ -1,20 +1,31 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+import librosa
+
+y, sr = librosa.load("PurpleLamborghini.aiff") 
+C=librosa.cqt(y,sr=sr).T[:]
+
 
 
 def update_line(num, data, line):
-    line.set_data(data[..., :num])
+    line.set_data(np.arange(0,data.shape[1]),data[num])
     return line,
 
 fig1 = plt.figure()
 
-data = np.random.rand(2, 25)
+data = np.abs(C)
 l, = plt.plot([], [], 'r-')
-plt.xlim(0, 1)
-plt.ylim(0, 1)
+plt.xlim(0, 84)
+plt.ylim(0, 10)
 plt.xlabel('x')
 plt.title('test')
-line_ani = animation.FuncAnimation(fig1, update_line, 2500, fargs=(data, l),
-                                   interval=50, blit=True)
+line_ani = animation.FuncAnimation(fig1, update_line, save_count=4000, fargs=(data, l),
+                                 interval=37, blit=True)
 line_ani.save('lines.mp4')
+
+
+
+
+
+
